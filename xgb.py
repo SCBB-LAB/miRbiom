fd138c96d63467f3593315972aa4878615ee6c00
Studio of Computational Biology and Bioinformatics <85485516+SCBB-LAB@users.noreply.github.com>
2021-10-15 12:04:59 +0530

    Update xgb.py

--- xgb.py
+++ xgb.py
@@ -1,5 +1,5 @@
 #!user/bin/python
-import sys, os
+import sys, os, math
 sys.path.append('/home/scbb/.local/lib/python2.7/site-packages')
 import pandas as pd
 import numpy as np
@@ -13,23 +13,31 @@ ensem_id=open('ensid')
 ensembl_gene_id=[line.rstrip("\n") for line in ensem_id ]
 for key in ensembl_gene_id:
 	if key not in d:
-		d[key]="NA"	
+		d[key]=0	
+
+
 mirnas=open('mirna-list-id')
 mirna_list = [line.rstrip("\n") for line in mirnas ]
 for mirna in mirna_list:
 	associated_gene='xgb_all/'+mirna.split(",")[0]+'_gene_ense_feature_new'
 	flist=open(associated_gene)
 	gene_list = [line.rstrip("\n") for line in flist ]
+	f=int(len(gene_list)*0.4)
 	try:
 		expression=[float(d[i]) for i in gene_list]
-		#pickle.dump(xg_reg, open(filename, 'wb')) #for saving the model
-		loaded_model = load(open('xgb_all/'+mirna.split(",")[0]+'_final_input_new.sav', 'rb'))
-		#df=pd.read_csv(mirna+'_final_input_data', sep="\t", header = None)
-		exp=[0]+expression
-		df = pd.DataFrame([exp])
-		X = df.iloc[:,1:]
-		y_load_predit=loaded_model.predict(X)
-		if y_load_predit[0]>0:
-			print mirna.split(",")[0],',',y_load_predit[0],',',mirna.split(",")[1],',',mirna.split(",")[2]	
+		expression1=[float(d[i]) for i in gene_list if float(d[i])>0]
+		h=int(len(expression1)/2)
+		if h>=f:
+			loaded_model = load(open('xgb_all/'+mirna.split(",")[0]+'_final_input_new.sav', 'rb'))
+			exp=[0]+expression
+			df = pd.DataFrame([exp])
+			X = df.iloc[:,1:]
+			y_load_predit=loaded_model.predict(X)
+			c=math.log(float(y_load_predit[0]),2)
+			if y_load_predit[0]>0:
+				print mirna.split(",")[0],',',y_load_predit[0],',',mirna.split(",")[1],',',mirna.split(",")[2],',',c
+		else:
+			print("More than 60% genes have no expression")
 	except:
 		print("Not sufficient genes")
+
